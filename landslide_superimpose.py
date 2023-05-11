54eca6ee9ec1fc3eef6a23978eb555ca5f60d2e2
Liam Toney <ldtoney@alaska.edu>
2023-05-11 14:10:43 -0800

    Make it obvious where to edit params in script

--- landslide_superimpose.py
+++ landslide_superimpose.py
@@ -8,12 +8,16 @@ import rioxarray
 from pyproj import CRS, Transformer
 from rasterio.enums import Resampling
 
+# -----------------------------------------------------------------------------
+# EDIT ME!
+# -----------------------------------------------------------------------------
 LANDSLIDE_IMAGE = '/Users/ldtoney/work/iliamna_avalanches/imagery/planet/2016_nir.tif'
 BACKGROUND_IMAGE = '/Users/ldtoney/school/defense/20220608_composite.tif'
 OUTLINE_FILE = '/Users/ldtoney/school/defense/iliamna_2016.kml'
 CROWN_COORDS = (60.0277, -153.0749)  # The crown of the landslide (from ESEC entry)
 TARGET_COORDS = (64.8595, -147.8489)  # Where we *place* the crown of the landslide
-PAD = 1000  # [m] Padding around outline for axis limits
+PAD = 1000  # [m] Padding around outline for axis limits of output image
+# -----------------------------------------------------------------------------
 
 # So we can read KML files?
 fiona.drvsupport.supported_drivers['LIBKML'] = 'rw'
